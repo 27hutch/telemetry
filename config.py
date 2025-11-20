379a1ffe6950b51b52a144e53971aadc5bdf09be
Jack Hutchins <hutchins.171@buckeyemail.osu.edu>
2025-11-19 20:03:14 -0500

    Added correct pin connections

--- config.py
+++ config.py
@@ -1,22 +1,29 @@
 # config.py
 
 # --- Radio / LoRa settings ---
-LORA_FREQ_MHZ = 915.0        # or 900.0 / 868.0 depending on your module + region
+LORA_FREQ_MHZ = 900.0        # or 900.0 / 868.0 depending on your module + region
 LORA_TX_POWER = 20           # dBm, check allowed limits for your country
-LORA_NODE_ID_ROCKET = 0x01
-LORA_NODE_ID_GROUND = 0x02
+
+# LORA_NODE_ID_ROVER = 0x01
+# LORA_NODE_ID_GROUND = 0x02
 
 # Optional: simple "network" ID so you don't pick up random packets
 LORA_NETWORK_ID = 0x42
 
 # --- SPI / GPIO pin mappings (BCM numbering) ---
-SPI_BUS = 0
-SPI_DEVICE = 0
+# SPI_BUS = 0
+# SPI_DEVICE = 0
 
 # RFM9x pins (adjust for your wiring)
-RFM9X_CS_PIN = 8      # CE0, for example
-RFM9X_RESET_PIN = 25
-RFM9X_IRQ_PIN = 24
+RFM9X_VIN = 1
+RFM9X_GND = 6
+RFM9X_MISO = 21
+RFM9X_SCK = 23
+RFM9X_MOSI = 19
+RFM9X_CS = 7
+RFM9X_RST = None #Specify by choosing a digital pin
+# RFM9X_RST 
+# RFM9X_G0
 
 # --- Telemetry settings ---
 TELEMETRY_HZ = 5.0    # send data 5 times per second from rocket
--- radio.py
+++ radio.py
@@ -7,27 +7,39 @@ import adafruit_rfm9x
 
 from config import (
     LORA_FREQ_MHZ,
-    LORA_TX_POWER_DBM,
-    RFM9X_CS_PIN_NAME,
-    RFM9X_RESET_PIN_NAME,
+    LORA_TX_POWER,
+    RFM9X_VIN,
+    RFM9X_GND,
+    RFM9X_MISO,
+    RFM9X_SCK,
+    RFM9X_MOSI,
+    RFM9X_CS,
+    RFM9X_RST
 )
 
 def init_radio() -> adafruit_rfm9x.RFM9x:
     """Initialize SPI + RFM9x and return the radio object."""
+    
     spi = busio.SPI(clock=board.SCK, MOSI=board.MOSI, MISO=board.MISO)
-
-    cs_pin = getattr(board, RFM9X_CS_PIN_NAME)
-    reset_pin = getattr(board, RFM9X_RESET_PIN_NAME)
+    
+    cs_pin = getattr(board, RFM9X_CS)
+    reset_pin = getattr(board, RFM9X_RST)
+    vin_pin = getattr(board, RFM9X_VIN)
+    gnd_pin = getattr(board, RFM9X_GND)
+    miso_pin = getattr(board, RFM9X_MISO)
+    sck_pin = getattr(board, RFM9X_SCK)
+    mosi_pin = getattr(board, RFM9X_MOSI)
 
     cs = digitalio.DigitalInOut(cs_pin)
     reset = digitalio.DigitalInOut(reset_pin)
+    vin = digitalio.DigitalInOut(vin_pin)
+    gnd = digitalio.DigitalInOut(gnd_pin)
+    miso = digitalio.DigitalInOut(miso_pin)
+    sck = digitalio.DigitalInOut(sck_pin)
+    mosi = digitalio.DigitalInOut(mosi_pin)
+
 
     radio = adafruit_rfm9x.RFM9x(spi, cs, reset, LORA_FREQ_MHZ)
-    radio.tx_power = LORA_TX_POWER_DBM
-
-    # Optional tuning for range/robustness:
-    # radio.signal_bandwidth = 125000   # Hz
-    # radio.coding_rate = 5
-    # radio.spreading_factor = 7
-
+    radio.tx_power = LORA_TX_POWER
+    
     return radio
